3550de86bfdc2256e09d67c779d3eb158a6c61c2
JManey <jdm216@gmail.com>
2020-06-23 14:50:52 -0500

    adds delete to todo list

--- jsBasicFunctions/pSet.js
+++ jsBasicFunctions/pSet.js
@@ -4,18 +4,18 @@
 //   } else return false;
 // }
 
-function isEven(num) {
-  return num % 2 === 0;
-}
+// function isEven(num) {
+//   return num % 2 === 0;
+// }
 
-function factorial(num) {
-  let result = 1;
-  for (let i = 2; i <= num; i++) {
-    result = result * i;
-  }
-  return result;
-}
+// function factorial(num) {
+//   let result = 1;
+//   for (let i = 2; i <= num; i++) {
+//     result = result * i;
+//   }
+//   return result;
+// }
 
-function kebabToSnake(str) {
-  return str.replace(/-/g, "_");
-}
+// function kebabToSnake(str) {
+//   return str.replace(/-/g, "_");
+// }
--- jsBasicFunctions/todoList.js
+++ jsBasicFunctions/todoList.js
@@ -2,13 +2,36 @@ let todos = [];
 
 let input = prompt("What would you like to do?");
 
+//logic
 while (input !== "quit") {
   if (input === "list") {
-    console.log(todos);
+    listTodos();
   } else if (input === "new") {
-    let newTodo = prompt("Enter todo:");
-    todos.push(newTodo);
+    addTodo();
+  } else if (input === "delete") {
+    deleteTodo();
   }
   input = prompt("What would you like to do?");
 }
 console.log("ok, you've exited the app");
+
+// functions
+function listTodos() {
+  console.log("*************");
+  todos.forEach(function (todo, idx) {
+    console.log(idx, ": ", todo);
+  });
+  console.log("*************");
+}
+
+function addTodo() {
+  let newTodo = prompt("Enter todo:");
+  todos.push(newTodo);
+  console.log("added: ", newTodo);
+}
+
+function deleteTodo() {
+  let idx = prompt("Enter number of todo to remove:");
+  let removed = todos.splice(idx, 1);
+  console.log("Deleted: ", removed[0]);
+}
