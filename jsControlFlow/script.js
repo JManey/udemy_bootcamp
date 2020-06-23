f92305b3cdb8d1225fa08b3f01a73d1170833f1c
JManey <jdm216@gmail.com>
2020-06-23 08:51:25 -0500

    completes control flow section

---
+++ jsControlFlow/script.js
@@ -0,0 +1,49 @@
+// //create secret number
+// const secretNumber = 4;
+
+// //ask user for guess
+// let guess = Number(prompt("Guess a number:"))
+
+// //check guess
+// if ((guess) === secretNumber) {
+//     alert("You guessed right!")
+// }
+
+// else if ((guess) > secretNumber) {
+//     alert("Too high! Guess again.")
+// }
+
+// else {
+//     alert("To low. Try again!")
+// }
+
+//while loops
+// let num1 = -10;
+// while (num1 <= 19) {
+//     console.log(num1)
+//     num1++
+// }
+
+// let num2 = 10;
+// while (num2 < 40) {
+//     if (num2 % 2 === 0) {
+//         console.log(num2)
+//     }
+//     num2++;
+// }
+
+let num4 = 5;
+while (num4 < 50) {
+    if (num4 % 15 === 0) {
+        console.log(num4)
+    }
+    num4++;
+}
+
+for (num = 5; num < 50; num++) {
+    if (num % 15 === 0) {
+        console.log(num)
+    }
+
+}
+
