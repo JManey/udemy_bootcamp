1126373de365e7758cfdefa75ba970029017fb69
JManey <jdm216@gmail.com>
2020-06-24 22:14:06 -0500

    making lots of colorful little circles

--- patatapClone/circles.js
+++ patatapClone/circles.js
@@ -1,7 +1,24 @@
+var circles = [];
+
 function onKeyDown(event) {
   var maxPoint = new Point(view.size.width, view.size.height);
   var randomPoint = Point.random();
   var point = maxPoint * randomPoint;
   // When a key is pressed, set the content of the text item:
-  new Path.Circle(point, 20).fillColor = "blue";
+  var newCircle = new Path.Circle(point, 20);
+  newCircle.fillColor = "blue";
+
+  circles.push(newCircle);
+}
+
+var animatedCircle = new Path.Circle(new Point(300, 300), 100);
+animatedCircle.fillColor = "red";
+
+function onFrame(event) {
+  for (var i = 0; i < circles.length; i++) {
+    // console.log(circles[i]);
+    circles[i].fillColor.hue += 1;
+  }
+  animatedCircle.fillColor.hue += 1;
+  //   animatedCircle.scale(0.95);
 }
