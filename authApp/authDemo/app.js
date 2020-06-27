8173c919397140984bda91960941ba552fff055a
JManey <jdm216@gmail.com>
2020-06-26 20:52:01 -0500

    register new user working using passport local, salt & hash saving to mongoDB

--- authApp/authDemo/app.js
+++ authApp/authDemo/app.js
@@ -20,20 +20,53 @@ app.use(
     saveUninitialized: false,
   })
 );
+// this is how to get rec.body working in express
+app.use(express.urlencoded({ extended: true })); //Parse URL-encoded bodies
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// ==================
+// Routes
+// ==================
+// home route
 app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`App listening on port: ${process.env.PORT}`);
-});
-
+// secret page protected route
 app.get("/secret", function (req, res) {
   res.render("secret");
+});
+
+// sign up form route
+app.get("/register", function (req, res) {
+  res.render("register");
+});
+
+//=====protected routes=======
+
+// handle user sign up
+app.post("/register", function (req, res) {
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secret");
+      });
+    }
+  );
+});
+
+//  =======================
+// ========================
+app.listen(process.env.PORT, () => {
+  console.log(`App listening on port: ${process.env.PORT}`);
 });
